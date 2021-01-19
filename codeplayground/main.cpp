af9f0d299fb8902d87382379cbfcd4d4949b284c
Victor Lopez <vjlopezcarrillo@gmail.com>
2021-01-19 16:33:55 -0700

    Initial Commit

---
+++ codeplayground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  codeplayground
+//
+//  Created by Greentwip on 19/01/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
