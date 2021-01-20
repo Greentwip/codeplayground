b44cad90a3f1fb6530d561eb9c561b11a6fc5f48
Victor Lopez <vjlopezcarrillo@gmail.com>
2021-01-19 20:05:16 -0700

    Greedy florist

--- codeplayground/structures/samples.cpp
+++ codeplayground/structures/samples.cpp
@@ -38,4 +38,31 @@ TEST_CASE( "Test products purchasen" ) {
     
 }
 
+TEST_CASE( "Test greedy florist" ) {
+    
+    auto c = std::vector<int>{1, 3, 7, 5, 9};
+    int k = 3;
+    
+    std::sort(c.begin(), c.end(), std::greater<int>());
+    
+    unsigned int count = (unsigned int)c.size();
+    int price = 0;
+    int previous_price = 0;
+    int customers = k;
+    
+    for(int flowers = 0, flowers_stage=customers-1; flowers<count; ++flowers){
+        price += c[flowers] * (previous_price + 1);
+        
+        if(flowers == flowers_stage){
+            previous_price++;
+            flowers_stage+=customers;
+        }
+        
+    }
+    
+    
+    CHECK(price == 29);
+    
+}
+
 
